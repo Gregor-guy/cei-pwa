5ddbbf3fe32c9e9378fc70f24673cdca3eaa800e
Gregor-guy <gregoryw@westbow.ca>
2026-08-17 17:31:03 -0700

    Add files via upload

--- netlify/functions/rss-feed.js
+++ netlify/functions/rss-feed.js
@@ -1,8 +1,7 @@
 exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "RSS Function Working"
-    })
-  };
+const articles=[
+{title:"Bank of Canada RSS Feed Connected",source:"Bank of Canada",link:"https://www.bankofcanada.ca/rss-feeds/"},
+{title:"CMHC RSS Feed Connected",source:"CMHC",link:"https://www.cmhc-schl.gc.ca/media-newsroom/cmhc-news-room-rss"}
+];
+return {statusCode:200,headers:{"Content-Type":"application/json"},body:JSON.stringify({articles})};
 };
